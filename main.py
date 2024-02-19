5ec52d20351c6cd333dc55c62f5aefd03995aa21
fadouaabdoul <fadouaabdoulmoulah@gmail.com>
2024-02-19 12:38:31 +0800

    gradient calcul

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
-
 import torch
+
+x = torch.randn(3, requires_grad=True)
+print(x)
+
+y = x + 2
+print(y)
+
+z = y*y*2
+z= z.mean()
+print(z)
+
+v = torch.tensor([0.1, 0.1, 0.001], dtype=torch.float32)
+z.backward() #dz/dx
+print(x.grad)
+
+#no grad use / and stop it from creating the grad function and the history graph
+x.requires_grad_(False)
+print(x)
+
+u = x.detach()
+print(u)
+
+with torch.no_grad():
+    b = x + 2
+    print(b)
